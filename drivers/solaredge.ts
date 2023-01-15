c2b5d30153e97146415e14acaa57bc6c5f084513
Edwin Biemond <biemond@gmail.com>
2023-01-15 12:58:52 +0100

    daily fix

--- drivers/solaredge.ts
+++ drivers/solaredge.ts
@@ -279,17 +279,21 @@ export class Solaredge extends Homey.Device {
                 var total = Number(result['energy_total'].value) * (Math.pow(10, Number(result['energy_total'].scale)));
                 this.setCapabilityValue('meter_power', total / 1000);
                 console.log(now);
-                if (this.getStoreValue("daily") == null ) {
-                    this.setStoreValue("daily",total / 1000 );
-                 } else if ( now == "24:01" || now == "24:02" || now == "00:01" || now == "00:02" ) {
-                    console.log("reset");
-                    console.log(total / 1000);
-                    this.setStoreValue("daily",total / 1000 );
-                 }
-                 var daily_power  = (total / 1000) -  this.getStoreValue("daily");
-                 console.log("daily: " + daily_power);
-                 this.addCapability('meter_power.daily');
-                 this.setCapabilityValue('meter_power.daily', daily_power);
+                if ( total != null ) {
+                    if (this.getStoreValue("daily") == null ) {
+                        this.setStoreValue("daily", total / 1000 );
+                    } else if ( now == "24:01" || now == "24:02" || now == "00:01" || now == "00:02" ) {
+                        console.log("reset");
+                        console.log(total / 1000);
+                        this.setStoreValue("daily", total / 1000 );
+                    }
+                    if (this.getStoreValue("daily") != null ) {
+                        var daily_power  = (total / 1000) -  this.getStoreValue("daily");
+                        console.log("daily: " + daily_power);
+                        this.addCapability('meter_power.daily');
+                        this.setCapabilityValue('meter_power.daily', daily_power);
+                    }
+                }
             }
 
             // if (result['power_dc'] && result['power_dc'].value != 'xxx' ){
