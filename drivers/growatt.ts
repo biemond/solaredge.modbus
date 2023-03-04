77d0fb571c5e075972c3aaf0af5487cea20cd7b2
Edwin Biemond <biemond@gmail.com>
2023-03-04 16:14:17 +0100

    update registers

--- drivers/growatt.ts
+++ drivers/growatt.ts
@@ -10,23 +10,33 @@ export class Growatt extends Homey.Device {
 
     registers: Object = {
 
-        "current": [0x9c87, 1, 'UINT16', "Current", -2 ], 
+        // "current":    [0x9c87, 1, 'UINT16', "Current", -2 ], 
+
+
         "l1_current": [0x9c88, 1, 'UINT16', "L1 Current", -2 ], 
-        "l2_current": [0x9c89, 1, 'UINT16', "L2 Current", -2 ], 
+        // "l1_current": [0x040F, 1, 'UINT16', "L1 Current", -1 ],         
+        // 39.	Iac1	Three/single phase grid output current		0.1A		78	1039	164	UINT	16	/10	A
+
+        "l2_current": [0x9c89, 1, 'UINT16', "L2 Current", -2 ],
+        // "l2_current": [0x0413, 1, 'UINT16', "L2 Current", -1 ],
+        // 43.	Iac2	Three phase grid output current		0.1A		86	1043	168	UINT	16	/10	A
+
         "l3_current": [0x9c8a, 1, 'UINT16', "L3 Current", -2 ], 
+        // "l3_current": [0x0417, 1, 'UINT16', "L3 Current", -1 ], 
+        // 47.	Iac3	Three phase grid output current		0.1A		94	1047	172	UINT	16	/10	A        
 
         "power_ac": [0x9c93, 1, 'INT16', "Power", -1],
-        "energy_total": [0x9c9d, 2, 'ACC32', "Total Energy", 0],
-        "current_dc": [0x9ca0, 1, 'UINT16', "DC Current", -4],
-
-        "voltage_dc": [0x9ca2, 1, 'UINT16', "DC Voltage", -1 ],
-
-        "power_dc": [0x9ca4, 1, 'INT16', "DC Power", -1 ],
+        // "power_ac": [0x0410, 2, 'UINT32', "Power", -1],
+        //40.	Pac1 H	Three/single phase grid output wattVA(high)		0.1VA		80	1040	165	UINT	32	/10	VA
+        //41.	Pac1 L	Three/single phase grid output watt VA(low)		0.1VA		82	1041	166				  
 
         "temperature": [0x9ca7, 1, 'INT16', "Temperature", -2],
+        // "temperature": [0x0445, 1, 'UINT16', "Temperature", -1],
+        //93.	Temp1	Inverter temperature		0.1C		186	1093	218	UINT	16	/10	C
 
-        "status": [0x9cab, 1, 'UINT16', "Status", 0],
-        "vendor_status": [0x9cac, 1, 'UINT16', "Vendor Status" ,0 ],
+        // "status": [0x9cab, 1, 'UINT16', "Status", 0],
+        //0.	Inverter Status	Inverter run state	0:waiting, 1:normal, 3:fault			0	1000	125	UINT	16	BIN	
+        // "status": [0x03e8, 1, 'UINT16', "Status", 0],        
 
     };
 
@@ -52,17 +62,17 @@ export class Growatt extends Homey.Device {
             }
             if (result['l1_current'] && result['l1_current'].value != '-1' && result['l1_current'].value != 'xxx') {
                 this.addCapability('measure_current.phase1');
-                var currenteac1 = Number(result['l1_current'].value) * (Math.pow(10,  Number(result['current'].scale)));
+                var currenteac1 = Number(result['l1_current'].value) * (Math.pow(10,  Number(result['l1_current'].scale)));
                 this.setCapabilityValue('measure_current.phase1', currenteac1);
             }
             if (result['l2_current'] && result['l2_current'].value != '-1' && result['l2_current'].value != 'xxx') {
                 this.addCapability('measure_current.phase2');
-                var currenteac2 = Number(result['l2_current'].value) * (Math.pow(10,  Number(result['current'].scale)));
+                var currenteac2 = Number(result['l2_current'].value) * (Math.pow(10,  Number(result['l2_current'].scale)));
                 this.setCapabilityValue('measure_current.phase2', currenteac2);
             }
             if (result['l3_current'] && result['l2_current'].value != '-1' && result['l3_current'].value != 'xxx') {
                 this.addCapability('measure_current.phase3');
-                var currenteac3 = Number(result['l3_current'].value) * (Math.pow(10,  Number(result['current'].scale)));
+                var currenteac3 = Number(result['l3_current'].value) * (Math.pow(10,  Number(result['l3_current'].scale)));
                 this.setCapabilityValue('measure_current.phase3', currenteac3);
             }
 
