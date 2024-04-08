8bed771c66c07567658a98242aaf81045bc75340
Edwin Biemond <biemond@gmail.com>
2024-04-08 21:09:49 +0200

    extra solax caps

--- drivers/solax.ts
+++ drivers/solax.ts
@@ -30,68 +30,83 @@ export class Solax extends Homey.Device {
         // // 0x001C Battery Capacity R Battery capacity 1% uint16 1
         // "BatteryCapacity":         [0x001C, 1, 'UINT16', "Battery Capacity"],
         // // 0x0020 OutputEnergy_Charge_today R OutputEnergy_Charge_today 0.1kWh uint16 1
-        // "OutputEnergy_Charge_today": [0x0020, 1, 'UINT16', "OutputEnergy_Charge_today"],
+        "OutputEnergy_Charge_today": [0x0020, 1, 'UINT16', "Battery Output Energy Today", -1],
         // // 0x0023 InputEnergy_Charge_today R InputEnergy_Charge_today 0.1kWh uint16 1
-        // "InputEnergy_Charge_today":  [0x0023, 1, 'UINT16', "InputEnergy_Charge_today"],
+        "InputEnergy_Charge_today":  [0x0023, 1, 'UINT16', "Battery Input Energy Today", -1],
 
-        // // 0x0046
-        // // feedin_power R
-        // // Feedin power is obtained from
-        // // Meter or CT.
-        // // (Postive mean generate
-        // // power; Negative mean
-        // // consumed power）
-        // // (0x46:LSB,0x47:MSB)
-        // // 1W int32 2
-        // "feedin_power":          [0x0046, 2, 'INT32', "Feedin power is obtained from Meter or CT"],
+        // 0x0046
+        // feedin_power R
+        // Feedin power is obtained from Meter or CT.
+        // (Postive mean generate power; Negative mean consumed power）
+        // (0x46:LSB,0x47:MSB)
+        // 1W int32 2
+        "feedin_power":          [0x0046, 2, 'INT32', "Feedin power is obtained from Meter or CT", 0],
 
-        // // 0x0048
-        // // feedin_energy_total(meter) R
-        // // energy to the grid
-        // // (0x48:LSB,0x49:MSB)
-        // // 0.01kWh uint32 2
-        // "feedin_energy_total":  [0x0048, 2, 'UINT32', "energy to the grid"],
-        // // 0x004A
-        // // consum_energy_total(meter) R
-        // // energy form the grid
-        // // (0x4A:LSB,0x4B:MSB)
-        // // 0.01kWh uint32 2
-        // "consum_energy_total":  [0x004A, 2, 'UINT32', "consum_energy_total"],
+        // 0x0048
+        // feedin_energy_total(meter) R
+        // energy to the grid
+        // (0x48:LSB,0x49:MSB)
+        // 0.01kWh uint32 2
+        "feedin_energy_total":  [0x0048, 2, 'UINT32', "energy to the grid", -2],
+        // 0x004A
+        // consum_energy_total(meter) R
+        // energy form the grid
+        // (0x4A:LSB,0x4B:MSB)
+        // 0.01kWh uint32 2
+        "consum_energy_total":  [0x004A, 2, 'UINT32', "energy from the grid", -2],
+
+        // ~0x0099
+        // feedin_energy_today
+        // R
+        // energy to the grid
+        // (meter)
+        // (0x98:LSB,0x99:MSB)
+        // 0.01kWh uint32
+        // 2
+        "feedin_energy_today":  [0x0098, 2, 'UINT32', "energy to the grid today", -2],
+        // ~0x009B
+        // consum_energy_today
+        // R
+        // energy form the grid
+        // (meter)
+        // (0x9A:LSB,0x9B:MSB)
+        // 0.01kWh uint16
+        "consum_energy_today":  [0x009A, 1, 'UINT16', "energy from the grid today", -2],
 
 
-        // // 0x0050 Etoday_togrid R
-        // // Today Energy
-        // // (Inverter AC Port)
-        // // 0.1kWh uint16 1
-        // "Etoday_togrid":        [0x0050, 1, 'UINT16', "Today's Solar Energy"],
+        // 0x0050 Etoday_togrid R
+        // Today Energy 
+        // (Inverter AC Port)
+        // 0.1kWh uint16 1
+        "Etoday_togrid":        [0x0050, 1, 'UINT16', "Today's Solar Energy", -1],
 
-        // // 0x0052
-        // // Etotal_togrid R
-        // // Total Energy
-        // // (Inverter AC Port)
-        // // (0x52:LSB,0x53:MSB)
-        // // 0.1kWh uint32 2
+        // 0x0052
+        // Etotal_togrid R
+        // Total Energy
+        // (Inverter AC Port)
+        // (0x52:LSB,0x53:MSB)
+        // 0.1kWh uint32 2
 
-        // "Etotal_togrid":  [0x0052, 2, 'UINT32', "Total Energy (Inverter AC Port)"],
+        "Etotal_togrid":  [0x0052, 2, 'UINT32', "Total Energy (Inverter AC Port)", -1],
 
-        // // 0x006A GridVoltage_R(X3) R GridVoltage_R 0.1V uint16 1
-        // "GridVoltage_R(X3)":        [0x006A, 1, 'UINT16', "GridVoltage_R(X3)"],
-        // // 0x006B GridCurrent_R(X3) R GridCurrent_R 0.1A int16 1
-        // "GridCurrent_R(X3)":        [0x006B, 1, 'INT16', "GridCurrent_R(X3)"],
-        // // 0x006C GridPower_R(X3) R GridPower_R 1W int16 1
-        // "GridPower_R(X3)":          [0x006C, 1, 'INT16', "GridPower_R(X3)"],
-        // // 0x006E GridVoltage_S(X3) R GridVoltage_S 0.1V uint16 1
-        // "GridVoltage_S(X3)":        [0x006E, 1, 'UINT16', "GridVoltage_S(X3)"],
-        // // 0x006F GridCurrent_S(X3) R GridCurrent_S 0.1A int16 1
-        // "GridCurrent_S(X3)":        [0x006F, 1, 'INT16', "GridCurrent_S(X3)"],
-        // // 0x0070 GridPower_S(X3) R GridPower_S 1W int16 1
-        // "GridPower_S(X3)":          [0x0070, 1, 'INT16', "GridPower_S(X3)"],
-        // // 0x0072 GridVoltage_T(X3) R GridVoltage_T 0.1V uint16 1
-        // "GridVoltage_T(X3)":        [0x0072, 1, 'UINT16', "GridVoltage_T(X3)"],
-        // // 0x0073 GridCurrent_T(X3) R GridCurrent_T 0.1A int16 1
-        // "GridCurrent_T(X3)":        [0x0073, 1, 'INT16', "GridCurrent_T(X3)"],
-        // // 0x0074 GridPower_T(X3) R GridPower_T 1W int16 1
-        // "GridPower_T(X3)":          [0x0074, 1, 'INT16', "GridPower_T(X3)"],
+        // 0x006A GridVoltage_R(X3) R GridVoltage_R 0.1V uint16 1
+        "GridVoltage_R(X3)":        [0x006A, 1, 'UINT16', "GridVoltage_R(X3)", -1],
+        // 0x006B GridCurrent_R(X3) R GridCurrent_R 0.1A int16 1
+        "GridCurrent_R(X3)":        [0x006B, 1, 'INT16', "GridCurrent_R(X3)", -1],
+        // 0x006C GridPower_R(X3) R GridPower_R 1W int16 1
+        "GridPower_R(X3)":          [0x006C, 1, 'INT16', "GridPower_R(X3)"],
+        // 0x006E GridVoltage_S(X3) R GridVoltage_S 0.1V uint16 1
+        "GridVoltage_S(X3)":        [0x006E, 1, 'UINT16', "GridVoltage_S(X3)", -1],
+        // 0x006F GridCurrent_S(X3) R GridCurrent_S 0.1A int16 1
+        "GridCurrent_S(X3)":        [0x006F, 1, 'INT16', "GridCurrent_S(X3)", -1],
+        // 0x0070 GridPower_S(X3) R GridPower_S 1W int16 1
+        "GridPower_S(X3)":          [0x0070, 1, 'INT16', "GridPower_S(X3)"],
+        // 0x0072 GridVoltage_T(X3) R GridVoltage_T 0.1V uint16 1
+        "GridVoltage_T(X3)":        [0x0072, 1, 'UINT16', "GridVoltage_T(X3)", -1],
+        // 0x0073 GridCurrent_T(X3) R GridCurrent_T 0.1A int16 1
+        "GridCurrent_T(X3)":        [0x0073, 1, 'INT16', "GridCurrent_T(X3)", -1],
+        // 0x0074 GridPower_T(X3) R GridPower_T 1W int16 1
+        "GridPower_T(X3)":          [0x0074, 1, 'INT16', "GridPower_T(X3)"],
 
         // // 0x0082
         // // ~0x0083
@@ -139,41 +154,25 @@ export class Solax extends Homey.Device {
         // // 0.1kWh uint32
         // // 2
         // "EchargeTotal":  [0x0092, 2, 'UINT32', "EchargeTotal"],
-        // // ~0x0095
-        // // SolarEnergyTotal
-        // // R
-        // // SolarEnergyTotal
-        // // (0x94:LSB,0x95:MSB)
-        // // 0.1kWh uint32
-        // // 2
-        // "SolarEnergyTotal":  [0x0094, 2, 'UINT32', "SolarEnergyTotal"],
-        // // 0x0096 SolarEnergyToday
-        // // R SolarEnergyToday 0.1kWh uint16
-        // // 1
-        // // 0x0097 REV
-        // // R
-        // // -
-        // // - uint16
-        // // 1
-        // "SolarEnergyToday":        [0x0096, 1, 'UINT16', "SolarEnergyToday"],
+        // ~0x0095
+        // SolarEnergyTotal
+        // R
+        // SolarEnergyTotal
+        // (0x94:LSB,0x95:MSB)
+        // 0.1kWh uint32
+        // 2
+        "SolarEnergyTotal":  [0x0094, 2, 'UINT32', "SolarEnergyTotal", -1],
+        // 0x0096 SolarEnergyToday
+        // R SolarEnergyToday 0.1kWh uint16
+        // 1
+        // 0x0097 REV
+        // R
+        // -
+        // - uint16
+        // 1
+        "SolarEnergyToday":  [0x0096, 1, 'UINT16', "SolarEnergyToday", -1],
 
-        // // ~0x0099
-        // // feedin_energy_today
-        // // R
-        // // energy to the grid
-        // // (meter)
-        // // (0x98:LSB,0x99:MSB)
-        // // 0.01kWh uint32
-        // // 2
-        // "feedin_energy_today":  [0x0098, 2, 'UINT32', "energy to the grid"],
-        // // ~0x009B
-        // // consum_energy_today
-        // // R
-        // // energy form the grid
-        // // (meter)
-        // // (0x9A:LSB,0x9B:MSB)
-        // // 0.01kWh uint16
-        // "consum_energy_today":  [0x009A, 1, 'UINT16', "energy from the grid"],
+
         // // 2
         // // 0x00BE BMS_UserSOC R BMS_UserSOC 1% Uint16 1
         // "BMS_UserSOC":        [0x00BE, 1, 'UINT16', "BMS_UserSOC"],
@@ -234,11 +233,24 @@ export class Solax extends Homey.Device {
                 var dcPower = Number(result['GridPower'].value) * (Math.pow(10, Number(result['GridPower'].scale)));
                 this.setCapabilityValue('measure_power', Math.round(dcPower));
             }
+
+
             if (result['Batpower_Charge1'] && result['Batpower_Charge1'].value != 'xxx') {
                 this.addCapability('measure_power.battery');
                 var dcPower = Number(result['Batpower_Charge1'].value) * (Math.pow(10, Number(result['Batpower_Charge1'].scale)));
                 this.setCapabilityValue('measure_power.battery', Math.round(dcPower));
             }
+            if (result['BatVoltage_Charge1'] && result['BatVoltage_Charge1'].value != 'xxx') {
+                this.addCapability('measure_current.batt_charge');
+                var dcPower = Number(result['BatVoltage_Charge1'].value) * (Math.pow(10, Number(result['BatVoltage_Charge1'].scale)));
+                this.setCapabilityValue('measure_current.batt_charge', Math.round(dcPower));
+            }
+            if (result['BatCurrent_Charge1'] && result['BatCurrent_Charge1'].value != 'xxx') {
+                this.addCapability('measure_voltage.batt_charge');
+                var dcPower = Number(result['BatCurrent_Charge1'].value) * (Math.pow(10, Number(result['BatCurrent_Charge1'].scale)));
+                this.setCapabilityValue('measure_voltage.batt_charge', Math.round(dcPower));
+            }            
+
             if (result['Powerdc1'] && result['Powerdc1'].value != 'xxx') {
                 this.addCapability('measure_power.pv1input');
                 var dcPower = Number(result['Powerdc1'].value) * (Math.pow(10, Number(result['Powerdc1'].scale)));
@@ -254,6 +266,120 @@ export class Solax extends Homey.Device {
                 var dcPower = Number(result['Temperature'].value) * (Math.pow(10, Number(result['Temperature'].scale)));
                 this.setCapabilityValue('measure_temperature.invertor', Math.round(dcPower));
             }     
+
+            if (result['OutputEnergy_Charge_today'] && result['OutputEnergy_Charge_today'].value != 'xxx') {
+                this.addCapability('meter_power.today_batt_output');
+                var dcPower = Number(result['OutputEnergy_Charge_today'].value) * (Math.pow(10, Number(result['OutputEnergy_Charge_today'].scale)));
+                this.setCapabilityValue('meter_power.today_batt_output', Math.round(dcPower));
+            }            
+            if (result['InputEnergy_Charge_today'] && result['InputEnergy_Charge_today'].value != 'xxx') {
+                this.addCapability('meter_power.today_batt_input');
+                var dcPower = Number(result['InputEnergy_Charge_today'].value) * (Math.pow(10, Number(result['InputEnergy_Charge_today'].scale)));
+                this.setCapabilityValue('meter_power.today_batt_input', Math.round(dcPower));
+            }  
+
+            if (result['feedin_power'] && result['feedin_power'].value != 'xxx') {
+                this.addCapability('measure_power.gridoutput');
+                var dcPower = Number(result['feedin_power'].value) * (Math.pow(10, Number(result['feedin_power'].scale)));
+                this.setCapabilityValue('measure_power.gridoutput', Math.round(dcPower));
+            }  
+
+            if (result['feedin_energy_total'] && result['feedin_energy_total'].value != 'xxx') {
+                this.addCapability('meter_power.export');
+                var dcPower = Number(result['feedin_energy_total'].value) * (Math.pow(10, Number(result['feedin_energy_total'].scale)));
+                this.setCapabilityValue('meter_power.export', Math.round(dcPower));
+            }  
+
+            if (result['consum_energy_total'] && result['consum_energy_total'].value != 'xxx') {
+                this.addCapability('meter_power.import');
+                var dcPower = Number(result['consum_energy_total'].value) * (Math.pow(10, Number(result['consum_energy_total'].scale)));
+                this.setCapabilityValue('meter_power.import', Math.round(dcPower));
+            }  
+
+            if (result['feedin_energy_today'] && result['feedin_energy_today'].value != 'xxx') {
+                this.addCapability('meter_power.export_daily');
+                var dcPower = Number(result['feedin_energy_today'].value) * (Math.pow(10, Number(result['feedin_energy_today'].scale)));
+                this.setCapabilityValue('meter_power.export_daily', Math.round(dcPower));
+            }  
+
+            if (result['consum_energy_today'] && result['consum_energy_today'].value != 'xxx') {
+                this.addCapability('meter_power.import_daily');
+                var dcPower = Number(result['consum_energy_today'].value) * (Math.pow(10, Number(result['consum_energy_today'].scale)));
+                this.setCapabilityValue('meter_power.import_daily', Math.round(dcPower));
+            }  
+
+            if (result['Etoday_togrid'] && result['Etoday_togrid'].value != 'xxx') {
+                this.addCapability('meter_power.daily');
+                var dcPower = Number(result['Etoday_togrid'].value) * (Math.pow(10, Number(result['Etoday_togrid'].scale)));
+                this.setCapabilityValue('meter_power.daily', Math.round(dcPower));
+            }  
+
+            if (result['Etotal_togrid'] && result['Etotal_togrid'].value != 'xxx') {
+                this.addCapability('meter_power');
+                var dcPower = Number(result['Etotal_togrid'].value) * (Math.pow(10, Number(result['Etotal_togrid'].scale)));
+                this.setCapabilityValue('meter_power', Math.round(dcPower));
+            }  
+
+            if (result['GridCurrent_R(X3)'] && result['GridCurrent_R(X3)'].value != 'xxx') {
+                this.addCapability('measure_current.meter_phase1');
+                var dcPower = Number(result['GridCurrent_R(X3)'].value) * (Math.pow(10, Number(result['GridCurrent_R(X3)'].scale)));
+                this.setCapabilityValue('measure_current.meter_phase1', Math.round(dcPower));
+            }  
+
+            if (result['GridCurrent_S(X3)'] && result['GridCurrent_S(X3)'].value != 'xxx') {
+                this.addCapability('measure_current.meter_phase2');
+                var dcPower = Number(result['GridCurrent_S(X3)'].value) * (Math.pow(10, Number(result['GridCurrent_S(X3)'].scale)));
+                this.setCapabilityValue('measure_current.meter_phase2', Math.round(dcPower));
+            }  
+
+            if (result['GridCurrent_T(X3)'] && result['GridCurrent_T(X3)'].value != 'xxx') {
+                this.addCapability('measure_current.meter_phase3');
+                var dcPower = Number(result['GridCurrent_T(X3)'].value) * (Math.pow(10, Number(result['GridCurrent_T(X3)'].scale)));
+                this.setCapabilityValue('measure_current.meter_phase3', Math.round(dcPower));
+            }  
+
+            if (result['GridVoltage_R(X3)'] && result['GridVoltage_R(X3)'].value != 'xxx') {
+                this.addCapability('measure_voltage.meter_phase1');
+                var dcPower = Number(result['GridVoltage_R(X3)'].value) * (Math.pow(10, Number(result['GridVoltage_R(X3)'].scale)));
+                this.setCapabilityValue('measure_voltage.meter_phase1', Math.round(dcPower));
+            }  
+
+            if (result['GridVoltage_S(X3)'] && result['GridVoltage_S(X3)'].value != 'xxx') {
+                this.addCapability('measure_voltage.meter_phase2');
+                var dcPower = Number(result['GridVoltage_S(X3)'].value) * (Math.pow(10, Number(result['GridVoltage_S(X3)'].scale)));
+                this.setCapabilityValue('measure_voltage.meter_phase2', Math.round(dcPower));
+            }  
+
+            if (result['GridVoltage_T(X3)'] && result['GridVoltage_T(X3)'].value != 'xxx') {
+                this.addCapability('measure_voltage.meter_phase3');
+                var dcPower = Number(result['GridVoltage_T(X3)'].value) * (Math.pow(10, Number(result['GridVoltage_T(X3)'].scale)));
+                this.setCapabilityValue('measure_voltage.meter_phase3', Math.round(dcPower));
+            }  
+
+
+            if (result['GridPower_R(X3)'] && result['GridPower_R(X3)'].value != 'xxx') {
+                this.addCapability('measure_power.meter_phase1');
+                var dcPower = Number(result['GridPower_R(X3)'].value) * (Math.pow(10, Number(result['GridPower_R(X3)'].scale)));
+                this.setCapabilityValue('measure_power.meter_phase1', Math.round(dcPower));
+            }  
+
+            if (result['GridPower_S(X3)'] && result['GridPower_S(X3)'].value != 'xxx') {
+                this.addCapability('measure_power.meter_phase2');
+                var dcPower = Number(result['GridPower_S(X3)'].value) * (Math.pow(10, Number(result['GridPower_S(X3)'].scale)));
+                this.setCapabilityValue('measure_power.meter_phase2', Math.round(dcPower));
+            }  
+
+            if (result['GridPower_T(X3)'] && result['GridPower_T(X3)'].value != 'xxx') {
+                this.addCapability('measure_power.meter_phase3');
+                var dcPower = Number(result['GridPower_T(X3)'].value) * (Math.pow(10, Number(result['GridPower_T(X3)'].scale)));
+                this.setCapabilityValue('measure_power.meter_phase3', Math.round(dcPower));
+            }  
+
+
+// EchargeToday: 1
+// EchargeTotal: 9043968
+// SolarEnergyTotal: 739246080
+// SolarEnergyToday: 104
 
         }
     }
