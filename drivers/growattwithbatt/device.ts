6aee2f940c64abd5e7c3442fbd2d7b4208cbf002
stanislav Voroniy <stas@voroniy.com>
2025-03-19 09:11:03 +0100

    One more fix of the same

--- drivers/growattwithbatt/device.ts
+++ drivers/growattwithbatt/device.ts
@@ -426,7 +426,7 @@ class MyGrowattBattery extends Growatt {
 
     socket.on('connect', async () => {
       console.log('Connected ...');
-      const limit = this.getCapabilityValue('exportlimitenabled');
+      const limit = Number(this.getCapabilityValue('exportlimitenabled'));
       const startRegister = startRegisters[type];
       if (limit === 1 && startRegister < 1100) {
         console.log('export limit is enabled, cannot change grid first time slot');
